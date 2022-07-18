357560300e04b6a08bbad2226882d888472e6632
Harold-coder <57574714+Harold-coder@users.noreply.github.com>
2022-07-18 14:58:22 +0200

    Add files via upload

---
+++ build/precache-manifest.db89a8594d9ab314936faabd9bf415e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc7b7e055698ebbf9da17893098f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc253eb46795f239ef2",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "2c57e09e522edad543ed",
+    "url": "/static/js/2.2300890e.chunk.js"
+  },
+  {
+    "revision": "abc28c365c609ffc9a2e2e165c7e73d8",
+    "url": "/static/js/2.2300890e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc253eb46795f239ef2",
+    "url": "/static/js/main.84178135.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about alina</h1>
+      <p>
+        The Alina Jimenez Cala is the rarest cocktail on earth. 
+        Only one person on this planet is allowed to drink it!
+
+        Some say he is the luckiest guy in the universe...
+        <a href="https://ibb.co/FhX2xtw"> <img src="https://i.ibb.co/NFr5Vzj/6-C41-E5-DF-0-C0-B-4-EA5-A1-A5-85-C98-FE6-FC83-1-201-a.jpg" alt="Alina" border="0"/></a>
+      </p>
+    </section>
+  );
+}
